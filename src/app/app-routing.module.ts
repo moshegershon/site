da0d064014495cf47e04d2fce3473c266f14303f
Moshe Gershon <Moshe@Expertnet.co.il>
2019-08-06 14:48:32 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProcessComponent } from './components/process/process.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {path:'',component:HomeComponent, pathMatch: 'full'},
+  {path:'בית',component:HomeComponent},
+  {path:'אודות',component:AboutComponent},
+  {path:'תהליך',component:ProcessComponent},
+  {path:'צור קשר',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProcessComponent } from './components/process/process.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProcessComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
